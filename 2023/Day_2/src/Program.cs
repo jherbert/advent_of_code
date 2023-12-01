b68c7eb69827c684fcc8470d2f210a5e795aef77
Jason Herbert <jason.herbert@q2.com>
2023-12-01 16:06:08 -0600

    Day 2 boiler plate

---
+++ 2023/Day_2/src/Program.cs
@@ -0,0 +1,11 @@
+﻿using Herbert.AdventOfCode.Common;
+using Herbert.AdventOfCode.Y2023.Day1;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddSingleton<Input>();
+builder.Services.AddSingleton<IPuzzle, Day2>();
+using IHost host = builder.Build();
+
+await host.Services.ExecutePuzzle(2023, 2);
---
+++ 2023/Day_2/test/Day2_Tests.cs
@@ -0,0 +1,32 @@
+namespace Herbert.AdventOfCode.Y2023.Day1.Tests;
+
+public class Day2_Tests
+{
+    [Theory]
+    [InlineData(
+        """
+        Test
+        """
+    )]
+    public async Task Solve_Puzzle(string input)
+    {
+        var puzzle = new Day2();
+        var result = await puzzle.Solve(input);
+
+        Assert.Equal("142", result);
+    }
+
+    [Theory]
+    [InlineData(
+        """
+        Test
+        """
+    )]
+    public async Task Solve_Puzzle_PartTwo(string input)
+    {
+        var puzzle = new Day2();
+        var result = await puzzle.Solve(input);
+
+        Assert.Equal("281", result);
+    }
+}
