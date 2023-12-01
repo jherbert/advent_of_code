51e29e0a8d8bc088c518bff8f28262f66f35cfd2
Jason Herbert <jason.herbert@q2.com>
2023-12-01 16:07:45 -0600

    Folder to copy for new days

---
+++ 2023/Copy/src/DayN.cs
@@ -0,0 +1,12 @@
+using Herbert.AdventOfCode.Common;
+
+namespace Herbert.AdventOfCode.Y2023.Day1
+{
+    public class DayN() : IPuzzle
+    {
+        public Task<string> Solve(string input)
+        {
+            return Task.FromResult("Hello World");
+        }
+    }
+}
---
+++ 2023/Copy/src/Program.cs
@@ -0,0 +1,11 @@
+﻿using Herbert.AdventOfCode.Common;
+using Herbert.AdventOfCode.Y2023.Day1;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddSingleton<Input>();
+builder.Services.AddSingleton<IPuzzle, DayN>();
+using IHost host = builder.Build();
+
+await host.Services.ExecutePuzzle(2023, 2);
